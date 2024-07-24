97d4fcd5590446fe93b6a1dacd3d7614a6878828
albertroig89 <albertroiglg@gmail.com>
2024-07-24 15:07:11 +0200

    chapter 3 finalized

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return 'Página de inicio';
+});
+
+Route::get('/notas', function () {
+    return 'Listado de notas';
+});
+
+Route::get('/notas/crear', function () {
+    return 'Crear nueva nota';
+});
+
+Route::get('cursos', function () {
+    return [
+        'Cursos' => [
+            'Curso de Laravel 11',
+            'Curso de programación orientada a objetos',
+            'Curso de Git',
+        ]
+    ];
 });
